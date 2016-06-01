dd49ef9de52f49220f71a9ffd40697ce6c554472
Stephen Done <srdone@gmail.com>
2016-05-31 22:01:04 -0600

    simple observable example

--- src/app/ng-2-observables.component.ts
+++ src/app/ng-2-observables.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Observable, Subject } from 'rxjs/Rx';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/pluck';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/combineLatest';
 
 @Component({
   moduleId: module.id,
@@ -6,6 +13,36 @@ import { Component } from '@angular/core';
   templateUrl: 'ng-2-observables.component.html',
   styleUrls: ['ng-2-observables.component.css']
 })
-export class Ng2ObservablesAppComponent {
-  title = 'ng-2-observables works!';
+export class Ng2ObservablesAppComponent implements OnInit {
+  addItemKeyup$ = new Subject<any>();
+  filterItemKeyup$ = new Subject<any>();
+  
+  allItems;
+  filteredItems;
+  
+  ngOnInit() {
+    var addItem$ = this.addItemKeyup$
+      .filter(x => x.keyIdentifier === 'Enter')
+      .pluck('target', 'value')
+      .startWith('John', 'Paul', 'Ringo');
+      
+    var filter$ = this.filterItemKeyup$
+      .pluck('target', 'value')
+      .map((x:any) => x.toLowerCase())
+      .startWith('');
+      
+    var allItem$ = addItem$
+      .scan((acc, val) => [...acc, val], [])
+      
+    allItem$.subscribe(x => this.allItems = x);
+      
+    allItem$.combineLatest(filter$, (items, filterValue) => {
+        if (filterValue === '') {
+          return items;
+        }
+        
+        return items.filter(i => i.toLowerCase().indexOf(filterValue) > -1);
+      }).subscribe(x => this.filteredItems = x);
+  }
+  
 }
